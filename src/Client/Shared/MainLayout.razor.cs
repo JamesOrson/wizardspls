6c37c1c883f83148cc3db66b764cfa2df2e50ad4
James Orson <jamesaorson@gmail.com>
2022-03-25 08:31:13 -0700

    Finish tanareallysucks

--- src/Client/Shared/MainLayout.razor.cs
+++ src/Client/Shared/MainLayout.razor.cs
@@ -1,45 +1,6 @@
-﻿using System;
-using Client.Config;
-using Microsoft.AspNetCore.Components;
-using Microsoft.JSInterop;
-
-namespace Client.Shared
+﻿namespace Client.Shared
 {
-    public partial class MainLayout : IDisposable
+	public partial class MainLayout
     {
-        #region Public
-
-        #region Member Methods
-        public void Dispose()
-        {
-            AppState.OnFaviconUriChange -= UpdateFavicon;
-            AppState.OnIsSideNavHiddenChange -= StateHasChanged;
-        }
-        #endregion
-
-        #endregion
-
-        #region Protected
-
-        #region Members
-        [Inject]
-        protected IJSRuntime _jsRuntime { get; set; }
-        #endregion
-
-        #region Member Methods
-        protected override void OnInitialized()
-        {
-            AppState.OnFaviconUriChange += UpdateFavicon;
-            AppState.OnIsSideNavHiddenChange += StateHasChanged;
-        }
-
-        protected void UpdateFavicon()
-        {
-            _jsRuntime.InvokeVoidAsync("updateFavicon", AppState.FaviconUri);
-            StateHasChanged();
-        }
-        #endregion
-
-        #endregion
     }
 }
